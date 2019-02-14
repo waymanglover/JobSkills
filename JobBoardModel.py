449237194c2fc784f137e934be69c0ccf7a26722
Wayman Glover <waymanglover@gmail.com>
2019-02-13 21:10:05 -0500

    Add simple docstrings to JobBoardModel

--- JobBoardModel.py
+++ JobBoardModel.py
@@ -1,3 +1,4 @@
+"""Class for representing a job board's state."""
 # Standard library dependencies
 import json
 from datetime import datetime, timedelta
@@ -5,7 +6,8 @@ from typing import Dict, Optional, IO
 
 
 class JobBoardModel():
-    """Represents a job board response"""
+    """Represents a job board response."""
+
     # Note: These are initialized partly to work around
     # a VS code linting/typing bug.
     # TODO: Find link to bug description
@@ -17,6 +19,7 @@ class JobBoardModel():
                  responseText: Optional[str] = None,
                  lastRequested: Optional[datetime] = None,
                  interval: Optional[int] = None) -> None:
+        """Create a JobBoardModel to represent the current job board state."""
         self.lastRequested = lastRequested
         if responseText is not None:
             self.responseText = responseText
@@ -24,10 +27,12 @@ class JobBoardModel():
             self.interval = interval
 
     def isReadyForRefresh(self) -> bool:
+        """Determine if we are ready to pull jobs from the job board."""
         nextRequestTime: datetime = self.getNextRefreshTime()
         return datetime.now() >= nextRequestTime
 
     def getNextRefreshTime(self) -> datetime:
+        """Return the next time we should request jobs from the job board."""
         if self.lastRequested is None or self.interval == 0:
             return datetime.now()
         delta: timedelta = timedelta(minutes=self.interval)
@@ -35,15 +40,18 @@ class JobBoardModel():
         return nextRequestTime
 
     def serialize(self, file: IO[str]) -> None:
+        """Dump the JSON representation of this object to a file."""
         json.dump(self.__dict__, file, default=JobBoardModel.jsonDefault)
 
     @classmethod
     def deserialize(cls, file: IO[str]) -> 'JobBoardModel':
+        """Read the JSON representation of a JobBoardModel into an object."""
         jobBoard: JobBoardModel = json.load(file, object_hook=cls.fromDict)
         return jobBoard
 
     @classmethod
     def fromDict(cls, dct: Dict[str, str]) -> 'JobBoardModel':
+        """Create a JobBoardModel from a dict containing its properties."""
         lastRequested: Optional[datetime] = None
         interval: Optional[int] = None
 
@@ -61,6 +69,7 @@ class JobBoardModel():
 
     @staticmethod
     def jsonDefault(obj: object) -> str:
+        """Use with json.dump to serialize classes with datetime properties."""
         if isinstance(obj, datetime):
             return obj.isoformat()
         raise TypeError(f'Type {type(obj)} not serializable')
